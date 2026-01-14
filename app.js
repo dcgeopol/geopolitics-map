d45379ac417c3da17081a31e3c9605af7bb6abf8
dcgeopol <dc.geopol@protonmail.com>
2026-01-13 18:21:19 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const map = L.map("map").setView([20, 0], 2);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap"
+}).addTo(map);
+
+let markers = [];
+
+fetch("data.json")
+  .then(res => res.json())
+  .then(data => {
+    const dateInput = document.getElementById("date");
+    dateInput.value = "2026-01-13";
+
+    function render(date) {
+      markers.forEach(m => map.removeLayer(m));
+      markers = [];
+
+      data.filter(d => d.date === date).forEach(d => {
+        const m = L.marker(d.coords)
+          .addTo(map)
+          .bindPopup(`<b>${d.country}</b><br>${d.type}<br>${d.text}`);
+        markers.push(m);
+      });
+    }
+
+    render(dateInput.value);
+    dateInput.addEventListener("change", e => render(e.target.value));
+  });
